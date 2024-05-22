a7c50749e0ca39dba07b1d7c096d95b6aac46944
Matheus Martins <matheushmar@gmail.com>
2024-05-21 21:25:01 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="font-[Inter] text-red-300">card consumer</h1>
+    <Button label="Primary" />
+    <Button label="Secondary" severity="secondary" />
+    <Button label="Success" severity="success" />
+    <Button label="Info" severity="info" />
+    <Button label="Warning" severity="warning" />
+    <Button label="Help" severity="help" />
+    <Button label="Danger" severity="danger" />
+    <Button label="Contrast" severity="contrast" />
+  </div>
+</template>
